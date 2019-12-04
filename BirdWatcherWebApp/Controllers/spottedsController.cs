ab354c6895aac896470359030e2abe6eb8b567c2
Sajan Pokhrel <Sajanlegend85@gmail.com>
2019-12-04 00:26:30 -0800

    changed variable

--- BirdWatcherWebApp/Controllers/spottedsController.cs
+++ BirdWatcherWebApp/Controllers/spottedsController.cs
@@ -54,7 +54,7 @@ namespace BirdWatcherWebApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("id,Bird,QuantitySpotted,longitude,latitude")] spotted spotted)
+        public async Task<IActionResult> Create([Bind("id,QuantitySpotted")] spotted spotted)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace BirdWatcherWebApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("id,Bird,QuantitySpotted,longitude,latitude")] spotted spotted)
+        public async Task<IActionResult> Edit(int id, [Bind("id,QuantitySpotted")] spotted spotted)
         {
             if (id != spotted.id)
             {
